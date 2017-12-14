36f79832627dcab54e0e596a1278702dedd849ca
Santosh Sindham <santoshkumar.sindham@in.tesco.com>
2017-12-14 14:51:06 +0530

    First commit

---
+++ src/main/java/com/bookselling/reactivedatastore/ReactiveDatastoreApplication.java
@@ -0,0 +1,12 @@
+package com.bookselling.reactivedatastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDatastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDatastoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bookselling/reactivedatastore/ReactiveDatastoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bookselling.reactivedatastore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveDatastoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
